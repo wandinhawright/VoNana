6fe5d105dcf93d523ae748d8634ee0689196192a
wandinhawright <wwanda.wwright@gmail.com>
2026-04-21 16:48:55 -0300

    criando arquivo da página institucional

---
+++ src/institucional.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './base/header.jsx';
+import Footer from './base/footer.jsx';
+import './base/index.css';
+
+function Institucional() {
+  return (
+    <div className="Institucional">
+      <Header />
+      <Footer /> 
+
+
+      <main style={{ padding: 'px' }}>
+      </main>
+    </div>
+  );
+}
+
+export default Institucional;
